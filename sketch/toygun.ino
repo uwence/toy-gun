dd493e3de0a1217d567ec30dc0a980a6c9720521
uwence <uwence@gmail.com>
2021-03-18 10:40:52 +0800

    change GunTiger to INPUT_PULLUP MODE

--- sketch/toygun.ino
+++ sketch/toygun.ino
@@ -6,26 +6,26 @@ int GunAccLevel=0;
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(GunTiger, INPUT); //gun triger
+  pinMode(GunTiger, INPUT_PULLUP); //gun triger
   pinMode(GunOuput, OUTPUT); //LED on Model A  or Pro
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
 
-    if (digitalRead(GunTiger) && !GunTigerEdge) {
+    if (!digitalRead(GunTiger) && !GunTigerEdge) {
         GunFiring=true;
     }
-    GunTigerEdge=digitalRead(GunTiger);
-    while (GunFiring || digitalRead(GunTiger)){
+    GunTigerEdge=!digitalRead(GunTiger);
+    while(GunFiring || !digitalRead(GunTiger)){
         digitalWrite(GunOuput,HIGH);
-        if(GunAccLevel<20){
+        if(GunAccLevel<2){
           GunAccLevel++;
           }
-        delay((500-(GunAccLevel*10)));
+        delay((500-(GunAccLevel*200)));
         digitalWrite(GunOuput,LOW);
         GunFiring=false;
-        delay(500-(GunAccLevel*10));
+        delay(500-(GunAccLevel*200));
     }
     GunAccLevel=0;
 }
